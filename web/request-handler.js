83420fae57dc694a2e4f08c760db669730cb06df
Hack Reactor Students <github@hackreactor.com>
2013-07-10 12:32:44 -0700

    Initial commit

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,26 @@
 exports.datadir = __dirname + "data/sites.txt"; // tests will need to override this.
+var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
   console.log(exports.datadir);
+
+  var statusCode;
+
+  if (req.method === 'GET') {
+    statusCode = 200;
+    //serve HTML for archived page
+    var siteHTML = fs.readFileSync('./data/sites/'+ userInput);
+    response.write('siteHTML', header);
+    response.end();
+  }
+
+  else if (req.method === 'POST') {
+    //append file on sites.txt
+    statusCode = 302;
+    var userInput
+  }
+
+  else {
+    //throw 404 or 406
+  }
 };
