cb2342216dc062cff1ee393450a0547eede1d7f3
Hack Reactor Students <github@hackreactor.com>
2013-07-10 14:43:35 -0700

    Server delivers default index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,20 +4,34 @@ var fs = require('fs');
 exports.handleRequest = function (req, res) {
   console.log(exports.datadir);
 
-  var statusCode;
+  var statusCode = 200;
+  var defaultCorsHeaders = {
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "Origin, Content-Type, Accept",
+    "access-control-max-age": 10, // Seconds.
+    "Content-Type": "text/html"
+  };
+  var html, styles;
+
+  if (req.url === '/') {
+    html = fs.readFileSync('./public/index.html');
+    res.writeHead(statusCode, defaultCorsHeaders);
+    res.write(html);
+    res.end();
+  }
 
   if (req.method === 'GET') {
-    statusCode = 200;
     //serve HTML for archived page
     var siteHTML = fs.readFileSync('./data/sites/'+ userInput);
-    response.write('siteHTML', header);
-    response.end();
+    res.writeHead(statusCode, header);
+    res.write(statusCode, header);
+    res.end('siteHTML');
   }
 
   else if (req.method === 'POST') {
     //append file on sites.txt
     statusCode = 302;
-    var userInput
   }
 
   else {
