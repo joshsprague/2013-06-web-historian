55d5760a9217dbdec027e04a52f491a27056d95e
Hack Reactor Students <github@hackreactor.com>
2013-07-11 12:09:15 -0700

    Passing all Worker specs

--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,4 +1,8 @@
 var fs = require('fs');
+var http = require('http');
+var path = require('path');
+
+var filePath = path.join(__dirname, '/testdata/sites/');
 
 exports.readUrls = function(filePath, cb){
   //read file at filepath
@@ -7,5 +11,13 @@ exports.readUrls = function(filePath, cb){
 };
 
 exports.downloadUrls = function(urls){
+  for (i = 0; i < urls.length; i++){
+    var destination = path.join(filePath, urls[i]);
+    http.get(urls[i], function(res) {
+      console.log(res.data);
+      fs.writeFileSync(destination, res);
+    });
+    return true;
+  }
   // fixme
 };
